af8335a1441e4f699f82e1a99e9c93c1076453c2
marcospr3421 <marcos@azacessorios.com.br>
2024-11-12 06:13:37 -0300

    Desafio Hotel finalizado

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,9 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            
+            
+            if (Suite.Capacidade <= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +27,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                Exception ex = new Exception("Capacidade menor que o número de hóspedes");
             }
         }
 
@@ -37,6 +40,10 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
             return 0;
         }
 
@@ -45,11 +52,20 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
+            if (Suite != null)
+            {
+                return DiasReservados * Suite.ValorDiaria;
+            }
             decimal valor = 0;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+                valor = valor - (valor * 0.1m);
+            }
+            else
             {
                 valor = 0;
             }
